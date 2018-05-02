d50bb13f3171a76d0c2c27e76679c41c413b3622
Sahlus <sahli.med.aziz@gmail.com>
2018-05-02 22:19:47 +0200

    Test

---
+++ java/com/example/GBanque/Form/SendMoneyForm.java
@@ -0,0 +1,20 @@
+package com.example.GBanque.Form;
+
+import lombok.Data;
+
+@Data
+public class SendMoneyForm {
+
+	private Long fromAccountId;
+	private Long toAccountId;
+	private Double amount;
+	public SendMoneyForm(Long fromAccountId, Long toAccountId, Double amount) {
+		this.fromAccountId = fromAccountId;
+		this.toAccountId = toAccountId;
+		this.amount = amount;
+	}
+	
+	
+	
+	
+}
---
+++ java/com/example/GBanque/Model/BanqueAccountInfo.java
@@ -0,0 +1,11 @@
+package com.example.GBanque.Model;
+
+import lombok.Data;
+
+@Data
+public class BanqueAccountInfo {
+
+	private Long id;
+	private String fullName;
+	private double balance;
+}
